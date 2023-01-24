95c640fc91eb6a5eed8090202ac3695dec79ea8c
zkratchman <61562769+zkratchman@users.noreply.github.com>
2023-01-24 16:43:28 -0500

    first commit

---
+++ pages/api/generate.js
@@ -0,0 +1,54 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix =
+`
+Take the following sentence from a resume and produce 5 options that focus on 1) making it more concise, 2) using more sophisiticated words and 3)highlight the results of the users actions:
+
+sentence:
+`
+
+const generateAction = async (req, res) => {
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.72,
+    max_tokens: 256,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  // I build Prompt #2.
+  const secondPrompt = 
+  `
+  Take the following five sentences and based on those sentences list out 3 sentences and 3 concise sentences that are all focused on different themes that are made for a professional resume and label them Juicy and concise sentences:
+
+  Sentences: ${basePromptOutput.text}
+
+  list:
+  `
+  
+  // I call the OpenAI API a second time with Prompt #2
+  const secondPromptCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${secondPrompt}`,
+    // I set a higher temperature for this one. Up to you!
+    temperature: 0.72,
+		// I also increase max_tokens.
+    max_tokens: 256,
+  });
+  
+  // Get the output
+  const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+  // Send over the Prompt #2's output to our UI instead of Prompt #1's.
+  res.status(200).json({ output: secondPromptOutput });
+};
+export default generateAction;
